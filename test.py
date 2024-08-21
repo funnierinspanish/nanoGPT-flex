ddfc86016f97be53af53d0e35af82bc46022082e
Diego Coy <diegocoyt@gmail.com>
2024-08-21 15:51:31 +0100

    update dir paths

--- test.py
+++ test.py
@@ -1,37 +1,38 @@
 import os
 from pathlib import Path
+import logging
 
-# Check if the output directory exists
-if Path("output").exists():
-    print("Output directory exists")
-else:
-    Path("output").mkdir(parents=True, exist_ok=True)
-    print("Created output/")
+# # Check if the output directory exists
+# if Path("output").exists():
+#     logging.debug("Output directory exists")
+# else:
+#     Path("output").mkdir(parents=True, exist_ok=True)
+#     logging.debug("Created output/")
 
-# Check if the input directory exists
-if Path("input").exists():
-    print("Input directory exists")
-else:
-    Path("input").mkdir(parents=True, exist_ok=True)
-    print("Created input/")
+# # Check if the root directory exists
+# if Path("input").exists():
+#     print("root directory exists")
+# else:
+#     Path("input").mkdir(parents=True, exist_ok=True)
+#     print("Created ")
 
 
-Path("input/subdir/more").mkdir(parents=True, exist_ok=True)
-print("Created input/subdir/more/")
+Path("subdir/more").mkdir(parents=True, exist_ok=True)
+logging.debug("Created subdir/more/")
 
-with open("input/this_was_in_input.txt", "w") as file:
-    file.write("This file was created in the Input directory.")
-    print("Created input/this_was_in_input.txt")
+with open("this_was_in_root.txt", "w") as file:
+    file.write("This file was created in the root directory.")
+    logging.debug("Created this_was_in_root.txt")
 
-with open("input/subdir/more/this_was_in_input.txt", "w") as file:
-    file.write("This file was created in the Input/subdir/more/ directory.")
-    print("Created input/subdir/more/this_was_in_input.txt")
+with open("subdir/more/this_was_in_root.txt", "w") as file:
+    file.write("This file was created in the subdir/more/ directory.")
+    logging.debug("Created subdir/more/this_was_in_root.txt")
 
 with open("output/new_output.txt", "w") as file:
     file.write("This file was created in the out directory.")
-    print("Created output/new_output.txt")
+    logging.debug("Created output/new_output.txt")
 
-os.system("cp input/this_was_in_input.txt output/copied_from_input.txt")
-print("Copied input/this_was_in_input.txt to output/copied_from_input.txt")
-os.system("cp input/subdir/more/this_was_in_input.txt output/copied_from_input_subdir.txt")
-print("Copied input/subdir/more/this_was_in_input.txt to output/copied_from_input_subdir.txt")
+os.system("cp this_was_in_root.txt output/copied_from_root.txt")
+logging.debug("Copied this_was_in_root.txt to output/copied_from_root.txt")
+os.system("cp subdir/more/this_was_in_root.txt output/copied_from_root_subdir.txt")
+logging.debug("Copied subdir/more/this_was_in_root.txt to output/copied_from_root_subdir.txt")
